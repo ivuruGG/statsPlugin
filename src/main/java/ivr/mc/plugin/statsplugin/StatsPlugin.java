7777e1c69a5163a750f1bf31048ebaf51a3afa44
ivuruGG <ivurug.dev@gmail.com>
2024-01-25 15:11:51 +0900

    Initial commit

---
+++ src/main/java/ivr/mc/plugin/statsplugin/StatsPlugin.java
@@ -0,0 +1,17 @@
+package ivr.mc.plugin.statsplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StatsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
